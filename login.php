b65926d567aa06eeb6cfd8efbf9d914e82f43586
smjosel <33595.j23@fje.edu>
2016-02-24 11:46:13 +0100

    login

---
+++ login.php
@@ -0,0 +1,6 @@
+<form name="f1" action="login.proc.php" method="get">
+	<input type="text" name="mail"placeholder="Correo"maxlength="50">
+    <input type="password" name="pass" class="form-control" placeholder="Contraseña">
+	<button type="submit" name="acce">Entrar</button>
+	<button type="button" onclick="">Registrarme</button>  
+</form>
