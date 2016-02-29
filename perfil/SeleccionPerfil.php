e61033e266734e1e86f73e400cc13e4e49ad9460
SanchezEric <8706.joan23@fje.edu>
2016-02-29 12:05:24 +0100

    Perfil
    Faltan rehacer las consultas

---
+++ perfil/SeleccionPerfil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="index.php">
+Selecciona Perfil: <input type="text" name="namePerfil"><br>
+<input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
